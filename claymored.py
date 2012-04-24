80f2782db717e13edab9db88d0a9ece510ef9b24
Ben Jackson <bbj@innismir.net>
2012-04-23 21:26:26 -0400

    Better graceful error checking

--- claymored.py
+++ claymored.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-import daemon
-
+import sys, os, daemon
 from claymore import claymore
+
+config_file = '/opt/claymore/claymore.ini'
+
+if not os.path.exists(config_file):
+	sys.exit("Unable to locate " + config_file + " -- Terminating")
 
 with daemon.DaemonContext():
 	claymore('/opt/claymore/claymore.ini')
